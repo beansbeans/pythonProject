1b3da2817f491f192e27ce0db4f993dbcd6f7cf8
Vyns Saenz <beanssaenz@gmail.com>
2024-06-13 18:09:17 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+numbers = [1,2,3,4,5]
+for item in numbers:
+    print(item)
+
+
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i = i+1
+#names = ["john", "bob", "mosh", "mary"]
+#replace the value of the set index into the defined value
+#names[0] = "jon"
+#prints the values of the list from index 0 to 2 (set value of index-1)
+#print(names[0:3])
+#prints the value of the index from the right
+#print(names[-2])
+
+#i = 1
+#while i <= 5:
+#    print(i * '*')
+#    i = i+1
+
+
+
+#print("Enter your weight below")
+#weight = float(input("Weight: "))
+#metricChoice = input("(K)g or (L)bs: ")
+#if metricChoice == 'k' or 'K':
+#    weightInLbs = weight * 2.2
+#    print("Your weight in pounds is: " + str(weightInLbs))
+#elif metricChoice == 'l' or 'L':
+#    weightInKg = weight / 2.2
+#    str(weightInKg)
+#    print("Your weight in kilogram is: " + str(weightInKg))
+#else:
+#    print("Invalid choice")
+#print("Thank you for using weight converter")
+
+
+
+#1 kg = 2.2 lbs
+
+# patientFirstName = "John"
+# patientLastName = "Smith"
+# patientAge = 20
+# isNewPatient = True
+#
+# print("Patient name is: ")
+# print(patientFirstName + " " + patientLastName)
+# print(patientAge)
+# print(isNewPatient)
+#
+# patientFullname = input("Enter patient name: ")
+# print("Patient name is: " + patientFullname)
+#
+# birthYear = input("Enter your year of birth: ")
+# age = 2024 - int(birthYear)
+#
+#
+# print(age)
+
+#https://youtu.be/kqtD5dpn9C8?t=1406
+# print("Calculator")
+# fnum = float(input("Enter first number: "))
+# snum = float(input("Enter second number: "))
+#
+# sum = fnum+snum
+#
+# print("the sum is "+ str(sum))
+
+#course = 'Python for beginners'
+#print(course.upper())
+#print(course.replace('y','z'))
+
+#price = 10
+#if price > 30:
+#    print("Price is greater than 30")
+#elif price == 10:
+#    print("price is exactly 10")
+#else:
+#    print("Price is less than 30")
+#print("That is all, thank you")
